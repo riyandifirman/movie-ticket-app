17aa692a2a59cfb5170e8ccdc6ad83499779bbc7
riyandifirman <riyandi.firman08@gmail.com>
2022-04-10 12:21:16 +0700

    Initial commit

---
+++ app/src/main/java/com/riyandifirman/movie/onboarding/OnboardingTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.movie.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyandifirman.movie.R
+
+class OnboardingTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_two)
+    }
+}
