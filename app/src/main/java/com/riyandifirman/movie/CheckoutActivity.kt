a5f9a0acc9759ce78cbf6e7147d0c5ec02ec7559
riyandifirman <riyandi.firman08@gmail.com>
2022-04-11 15:46:55 +0700

    add checkout, edit profile, my wallet, and history page layout

---
+++ app/src/main/java/com/riyandifirman/movie/CheckoutActivity.kt
@@ -0,0 +1,11 @@
+package com.riyandifirman.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
